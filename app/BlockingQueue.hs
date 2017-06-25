e42440644795008e58ea6918bf98f9348aa402d7
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-06-26 01:54:59 +0530

    define the basic state

--- app/BlockingQueue.hs
+++ app/BlockingQueue.hs
@@ -5,6 +5,7 @@ module BlockingQueue where
 import Data.Typeable
 import GHC.Generics
 import Data.Binary
+import Control.Distributed.Process.Async
 import Control.Distributed.Process hiding (call)
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node
@@ -13,9 +14,18 @@ import Control.Distributed.Process.Extras.Time
 import Control.Distributed.Process.Serializable
 import Control.Distributed.Process.Extras.Internal.Types
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Data.Sequence
 
 executeTask :: forall s a . (Addressable s, Serializable a)
             => s
             -> Closure (Process a)
             -> Process (Either String a)
 executeTask = call
+
+type SizeLimit = Int
+
+data BlockingQueue a = BlockingQueue {
+  poolSize :: SizeLimit ,
+  active :: [(MonitorRef, CallRef (Either ExitReason a), Async a)] ,
+  accepted :: Seq (CallRef (Either ExitReason a), Closure (Process a))
+  }
